b22e04f73e1d4b6b178d82bb4a8de8fa026bbf4c
MuneebRashid123 <152770592+MuneebRashid123@users.noreply.github.com>
2025-05-31 18:45:34 +0000

    basics comit config

---
+++ basics/ConversionOperator.js
@@ -0,0 +1,7 @@
+let score="33abc";
+console.log(typeof score);
+console.log(typeof (score));
+let ValueInNumber=Number(score);
+console.log(typeof ( ValueInNumber));
+console.log(ValueInNumber);// it  give  number   type  and  value 33
+console.log(ValueInNumber);// When I  write  value in score""33abc" and it give output NAN notanNumber it is special type value na ye to mhi dya ha
--- basics/Variable.js
+++ basics/Variable.js
@@ -2,6 +2,8 @@ const  accountId=133335;
 let accountEmail="muneebrashid880@gmail.com";
 var accountpassword = 3333 ;
 accountcity="mumbai";
+let accountState;//value undefined 
+ 
 // memmory ka andar assign kardo
 
 // accountId =2; not allowed 
@@ -12,3 +14,5 @@ accountcity="hyderabad";
 console.table([accountEmail,accountId,accountpassword,accountcity]);
 // memory ma space reserved hogi ha jo mere  value ko  store karahe ha
 // tabular struture ma variable ayenga
+/*  perfer not to use  var
+   because of issue in block scope and  functional scope   */
