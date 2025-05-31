f251e53ee8492f148d2995bc956b9a74ff24e32f
MuneebRashid123 <152770592+MuneebRashid123@users.noreply.github.com>
2025-05-31 11:46:40 +0000

    basics config done

---
+++ basics/Variable.js
@@ -0,0 +1,14 @@
+const  accountId=133335;
+let accountEmail="muneebrashid880@gmail.com";
+var accountpassword = 3333 ;
+accountcity="mumbai";
+// memmory ka andar assign kardo
+
+// accountId =2; not allowed 
+// console.log(accountId);
+accountEmail="muneebrashid887@gmail.com";
+accountpassword=4444;
+accountcity="hyderabad";
+console.table([accountEmail,accountId,accountpassword,accountcity]);
+// memory ma space reserved hogi ha jo mere  value ko  store karahe ha
+// tabular struture ma variable ayenga
