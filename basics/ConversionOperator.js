ca8e64e96412311e74443b1a5d792b876154cdd9
MuneebRashid123 <152770592+MuneebRashid123@users.noreply.github.com>
2025-06-01 13:50:19 +0000

    basics comit config done

--- basics/ConversionOperator.js
+++ basics/ConversionOperator.js
@@ -1,7 +1,25 @@
-let score="33abc";
+let score="abc";
 console.log(typeof score);
 console.log(typeof (score));
 let ValueInNumber=Number(score);
 console.log(typeof ( ValueInNumber));
 console.log(ValueInNumber);// it  give  number   type  and  value 33
-console.log(ValueInNumber);// When I  write  value in score""33abc" and it give output NAN notanNumber it is special type value na ye to mhi dya ha
+console.log(ValueInNumber);// When I  write  value in score""33abc" and it give output NAN notanNumber it is special type value , no ne ye to nhi dya ha
+// convert  nhi hota to NAN
+// null is 0  , undefined  is NAN
+//  boolean true = 1  , false =0
+// "33abc"==> NAN type of NAN is Number
+let isLoggedIn=1;
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1  = true
+// 0 = false
+// " " = false
+// "hitesh"==>true
+
+
+let somenumber=33;
+let StringNumber=String(somenumber);
+console.log(StringNumber);
+console.log( typeof (StringNumber));
---
+++ basics/Stringno.js
@@ -0,0 +1,9 @@
+let val=3;
+let negval= -val;
+console.log(negval);
+//  =-3
+let str1= "Muneeb";
+let str2="Khan";
+let str3= str1+str2;
+console.log(str3);
+
