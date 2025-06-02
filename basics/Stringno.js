59916d7c01e58c4bab30b26bcfb2da3145e4d218
MuneebRashid123 <152770592+MuneebRashid123@users.noreply.github.com>
2025-06-02 12:12:56 +0000

    basics config done

--- basics/Stringno.js
+++ basics/Stringno.js
@@ -2,8 +2,49 @@ let val=3;
 let negval= -val;
 console.log(negval);
 //  =-3
+// let str1= "Muneeb";
+// let str2="Khan";
+// let str3= str1+str2;
+// console.log(str3);
 let str1= "Muneeb";
 let str2="Khan";
-let str3= str1+str2;
-console.log(str3);
+//  (bactect) string interpolation    placeholder banata  ha   jo be variable ha direct inject karsata ha 
+console.log(`hello my name is ${str1}  and  my  cast is ${str2}`);
+
+
+console.log(str1.length);
+console.log(str1.toUpperCase());
+console.log(str1.charAt(3));// kis index ma konsa character ha
+// The charAt() method is used to get a specific character from a string based on its index
+console.log(str1.indexOf('e'));//konsa  character konsi position ma ha
+// indexOf() is used to find the position of a specific character or substring inside a string or array
+console.log(str1.substring(0 , 4))// start  from 0 and end  in 3
+console.log(str1.slice(-4 , 4))
+// Your string: "Muneeb"
+// Length = 6
+// -4 → 6 - 4 = 2
+
+
+// trim remove starting space and endind space 
+const newStringOne = "    Hi/n tesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url ="https://muneeb.com/muneeb%20choudhary";
+console.log(url.replace("%20","-"));
+
+
+console.log(url.includes("mammasdhsdjfhdrjhkfsdbb"));
+console.log(url.includes("muneeb"));
+
+
+// The split() method is used to break a string into parts and return those parts in an array
+
+let str = "Muneeb is learning JS";
+let result = str.split(" ",2);
+
+console.log(result);
+// Output: ["Muneeb", "is", "learning", "JS"]
+
+
 
---
+++ basics/datatypes-summary.js
@@ -0,0 +1,11 @@
+// Primitive datatype
+// 7 types : String , Number , Boolean , null, undefined, Symbol, Bigint
+// it is  call by value jab be ap kahe sa kahe copy karta ha is ka original data ka reference nhi dya jata 
+// inka ap copy karka apko dya jata ha
+// jo be ap changes karta ho inka copy ma changes hota ha
+
+
+// Non Primitive datatype / reference type
+// inka reference direct  allocate  kya jasata  ha
+// Array, Objects, Functions 
+// javascript  ko hum language ma define nhin karta hav
